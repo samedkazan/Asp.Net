b30b051ada4070bafd852d92b41bb9960fd65cc8
Samed <sametkazan92@gmail.com>
2022-11-08 17:53:21 +0300

    Mvc Project Task One

---
+++ TaskOne/MvcProjeKampi/BusinessLayer/Conctrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Conctrete
+{
+    public class CategoryManager
+    {
+        GenericRepositroy<Category> repo = new GenericRepositroy<Category>();
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBL(Category p)
+        {
+            if(p.CategoryName==""||p.CategoryName.Length<=3||
+                p.CategoryDescription == "" || p.CategoryName.Length >= 51)
+            {
+               //Hata Mesajı
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+}
